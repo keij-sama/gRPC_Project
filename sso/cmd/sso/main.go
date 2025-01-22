38e6bea0fc4fde540ac95434cb73cc217c3da30a
keij <skakovadrawing@gmail.com>
2025-01-22 23:19:39 +0300

    logger

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -1,11 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/keij-sama/gRPC_Project/sso/iternal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
-	// TODO: инициализировать объект конфига
+
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("prot", cfg.GRPC.Port),
+	)
+
+	log.Debug("debug message")
+	log.Error("error message")
+	log.Warn("warn message")
+
+	fmt.Println(cfg)
 
 	// TODO: инициализировать логгер
 
 	// TODO: инициализировать приложение (app)
 
 	// TODO: запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
